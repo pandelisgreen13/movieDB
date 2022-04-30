41d09785c189f4fa6dfbb4a63c34b2ee4af8f17c
Pantelis Chasapis <p.chasapis@afse.eu>
2022-04-30 17:30:45 +0300

    fix list design, add error view

--- app/src/main/java/gr/pchasapis/moviedb/ui/fragment/favourite/FavouriteFragment.kt
+++ app/src/main/java/gr/pchasapis/moviedb/ui/fragment/favourite/FavouriteFragment.kt
@@ -4,17 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
@@ -22,17 +14,20 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.AndroidEntryPoint
 import gr.pchasapis.moviedb.databinding.FavouriteFragmentBinding
-import gr.pchasapis.moviedb.model.data.HomeDataModel
 import gr.pchasapis.moviedb.ui.compose.MovieDBTheme
-import gr.pchasapis.moviedb.ui.compose.PrimaryDark
-import gr.pchasapis.moviedb.ui.fragment.favourite.card.FavouriteRow
+import gr.pchasapis.moviedb.ui.fragment.favourite.card.FavouriteList
+import gr.pchasapis.moviedb.ui.fragment.favourite.card.LoadingErrorCompose
 
 @AndroidEntryPoint
 class FavouriteFragment : Fragment() {
 
     private var binding: FavouriteFragmentBinding? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return ComposeView(requireContext()).apply {
 
             setContent {
@@ -40,18 +35,17 @@ class FavouriteFragment : Fragment() {
                 MovieDBTheme {
 
                     Surface(
-                            modifier = Modifier.fillMaxSize(),
-                            color = MaterialTheme.colors.primary) {
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.primary
+                    ) {
 
                         val viewModel = viewModel<FavouriteViewModel>()
                         val state = viewModel.state
                         val list = state.initialFavourite
 
-                        if (state.loading) {
-                            EmptyCompose()
-                        }
-
-                        if (list.isNotEmpty()) {
+                        if (state.loading || list.isEmpty()) {
+                            LoadingErrorCompose(shouldShowError = state.loading.not() && list.isEmpty())
+                        } else {
                             FavouriteList(messages = list)
                         }
                     }
@@ -60,35 +54,8 @@ class FavouriteFragment : Fragment() {
         }
     }
 
-    @Composable
-    fun FavouriteList(messages: List<HomeDataModel>) {
-        LazyColumn {
-            items(messages) { favourite ->
-                FavouriteRow(homeDataModel = favourite)
-            }
-        }
-    }
-
-    @Composable
-    fun EmptyCompose() {
-        Surface(
-                color = PrimaryDark,
-                modifier = Modifier.fillMaxSize()
-        ) {
-            Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                CircularProgressIndicator()
-            }
-        }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         binding = null
     }
-
 }
--- app/src/main/java/gr/pchasapis/moviedb/ui/fragment/favourite/card/FavouriteCard.kt
+++ app/src/main/java/gr/pchasapis/moviedb/ui/fragment/favourite/card/FavouriteCard.kt
@@ -3,13 +3,21 @@ package gr.pchasapis.moviedb.ui.fragment.favourite.card
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -26,11 +34,9 @@ import gr.pchasapis.moviedb.ui.fragment.details.MovieImage
 @Composable
 fun FavouriteRow(homeDataModel: HomeDataModel) {
     Card(
-            backgroundColor = PrimaryDark,
-            shape = RoundedCornerShape(8.dp),
-            elevation = 8.dp,
-            modifier = Modifier
-                    .padding(vertical = 6.dp, horizontal = 4.dp)
+        backgroundColor = PrimaryDark,
+        shape = RoundedCornerShape(8.dp),
+        elevation = 8.dp,
     ) {
         FavouriteContent(homeDataModel = homeDataModel)
     }
@@ -38,48 +44,111 @@ fun FavouriteRow(homeDataModel: HomeDataModel) {
 
 @Composable
 fun FavouriteContent(homeDataModel: HomeDataModel) {
-   // var expanded by rememberSaveable { mutableStateOf(false) }
+    // var expanded by rememberSaveable { mutableStateOf(false) }
 
     Row(
-            modifier = Modifier
-                    .animateContentSize(
-                            animationSpec = spring(
-                                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                                    stiffness = Spring.StiffnessLow
-                            )
-                    )
+        modifier = Modifier
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
 
         MovieImage(
-                homeDataModel.thumbnail
+            homeDataModel.thumbnail
         )
 
         Column(
-                modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp)
         ) {
 
             ComposeText(
-                    text = homeDataModel.title ?: "-",
-                    maxLines = 2,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(top = 6.dp)
+                text = homeDataModel.title ?: "-",
+                maxLines = 2,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 6.dp)
             )
 
             ComposeText(
-                    text = "${stringResource(R.string.home_release_data)}  ${homeDataModel.releaseDate}",
-                    fontSize = 14.sp,
-                    modifier = Modifier.padding(top = 10.dp)
+                text = "${stringResource(R.string.home_release_data)}  ${homeDataModel.releaseDate}",
+                fontSize = 14.sp,
+                modifier = Modifier.padding(top = 10.dp)
             )
 
             ComposeText(
-                    text = "${stringResource(R.string.home_rating)}  ${homeDataModel.ratings}",
-                    fontSize = 14.sp,
-                    modifier = Modifier.padding(top = 4.dp)
+                text = "${stringResource(R.string.home_rating)}  ${homeDataModel.ratings}",
+                fontSize = 14.sp,
+                modifier = Modifier.padding(top = 4.dp)
             )
         }
+    }
+}
+
+@Composable
+fun FavouriteList(messages: List<HomeDataModel>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        contentPadding = PaddingValues(horizontal = 14.dp, vertical = 8.dp)
+    ) {
+        items(messages) { favourite ->
+            FavouriteRow(homeDataModel = favourite)
+        }
+    }
+}
+
+@Composable
+fun LoadingErrorCompose(shouldShowError: Boolean = false) {
+    Surface(
+        color = PrimaryDark,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            if (shouldShowError) {
+                ComposeText(text = stringResource(id = R.string.favourite_empty_list))
+            } else {
+                CircularProgressIndicator()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ListPreview() {
+    MovieDBTheme {
+        val list = arrayListOf(
+            HomeDataModel(
+                ratings = "5",
+                title = "Avengers",
+                releaseDate = "25/5/2019"
+            ),
+            HomeDataModel(
+                ratings = "5",
+                title = "Avengers222",
+                releaseDate = "25/5/2019"
+            ),
+            HomeDataModel(
+                ratings = "5",
+                title = "Avengers222",
+                releaseDate = "25/5/2019"
+            ),
+            HomeDataModel(
+                ratings = "5",
+                title = "Avengers222",
+                releaseDate = "25/5/2019"
+            )
+        )
+        FavouriteList(list)
     }
 }
 
@@ -87,11 +156,29 @@ fun FavouriteContent(homeDataModel: HomeDataModel) {
 @Composable
 fun CardPreview() {
     MovieDBTheme {
-        FavouriteRow(HomeDataModel(
+        FavouriteRow(
+            HomeDataModel(
                 ratings = "5",
                 title = "Avengers",
                 releaseDate = "25/5/2019"
-        ))
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingPreview() {
+    MovieDBTheme {
+        LoadingErrorCompose()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorPreview() {
+    MovieDBTheme {
+        LoadingErrorCompose(true)
     }
 }
 
