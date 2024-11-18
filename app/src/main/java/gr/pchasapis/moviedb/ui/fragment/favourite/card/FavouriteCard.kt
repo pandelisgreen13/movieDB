2577e14e4747fa24014beaca514cc647e2125cb4
Pantelis Chasapis <panteleimon.chasapis1@vodafone.com>
2024-11-18 17:09:16 +0200

    add keys to composables

--- app/src/main/java/gr/pchasapis/moviedb/ui/fragment/favourite/card/FavouriteCard.kt
+++ app/src/main/java/gr/pchasapis/moviedb/ui/fragment/favourite/card/FavouriteCard.kt
@@ -11,15 +11,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -54,6 +51,7 @@ fun FavouriteRow(
         FavouriteContent(homeDataModel = homeDataModel)
     }
 }
+
 @Composable
 fun FavouriteGrid(
     homeDataModel: HomeDataModel,
@@ -132,7 +130,7 @@ fun FavouriteList(
         horizontalArrangement = Arrangement.spacedBy(10.dp),
         columns = GridCells.Adaptive(100.dp)
     ) {
-        items(messages) { favourite ->
+        items(messages, key = { favourite -> favourite.id!! }) { favourite ->
             FavouriteGrid(homeDataModel = favourite) {
                 onItemClicked.invoke(it)
             }
@@ -169,22 +167,26 @@ fun ListPreview() {
             HomeDataModel(
                 ratings = "5",
                 title = "Avengers",
-                releaseDate = "25/5/2019"
+                releaseDate = "25/5/2019",
+                id = 5
             ),
             HomeDataModel(
                 ratings = "5",
                 title = "Avengers222",
-                releaseDate = "25/5/2019"
+                releaseDate = "25/5/2019",
+                id = 6
             ),
             HomeDataModel(
                 ratings = "5",
                 title = "Avengers222",
-                releaseDate = "25/5/2019"
+                releaseDate = "25/5/2019",
+                id = 7
             ),
             HomeDataModel(
                 ratings = "5",
                 title = "Avengers222",
-                releaseDate = "25/5/2019"
+                releaseDate = "25/5/2019",
+                id = 8
             )
         )
         FavouriteList(list, {})
@@ -199,7 +201,8 @@ fun CardPreview() {
             HomeDataModel(
                 ratings = "5",
                 title = "Avengers",
-                releaseDate = "25/5/2019"
+                releaseDate = "25/5/2019",
+                id = 5
             )
         ) {}
     }
--- app/src/main/java/gr/pchasapis/moviedb/ui/fragment/home/compose/HomeScreen.kt
+++ app/src/main/java/gr/pchasapis/moviedb/ui/fragment/home/compose/HomeScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -31,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -129,7 +127,10 @@ fun HomeList(messages: Flow<PagingData<HomeDataModel>>, onItemClicked: (HomeData
         contentPadding = PaddingValues(horizontal = 14.dp, vertical = 14.dp)
     ) {
         items(
-            lazyPagingItems.itemCount
+            lazyPagingItems.itemCount,
+            key = {
+                lazyPagingItems[it]?.id!!
+            }
         ) {
             val favourite = lazyPagingItems[it]!!
             FavouriteRow(homeDataModel = favourite) { model ->
@@ -189,13 +190,13 @@ fun PreviewHome() {
     MovieDBTheme {
         val list = arrayListOf(
             HomeDataModel(
-                ratings = "5", title = "Avengers", releaseDate = "25/5/2019"
+                ratings = "5", title = "Avengers", releaseDate = "25/5/2019", id = 5
             ), HomeDataModel(
-                ratings = "5", title = "Avengers222", releaseDate = "25/5/2019"
+                ratings = "5", title = "Avengers222", releaseDate = "25/5/2019", id = 6
             ), HomeDataModel(
-                ratings = "5", title = "Avengers222", releaseDate = "25/5/2019"
+                ratings = "5", title = "Avengers222", releaseDate = "25/5/2019", id = 7
             ), HomeDataModel(
-                ratings = "5", title = "Avengers222", releaseDate = "25/5/2019"
+                ratings = "5", title = "Avengers222", releaseDate = "25/5/2019", id = 8
             )
         )
         val pagingData = PagingData.from(list)
