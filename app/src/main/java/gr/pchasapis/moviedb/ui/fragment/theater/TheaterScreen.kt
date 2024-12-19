2e4753b8c65223198e95943f52edba5d3c09697c
Pantelis Chasapis <panteleimon.chasapis1@vodafone.com>
2024-12-19 16:33:00 +0200

    add grid to screen

--- app/src/main/java/gr/pchasapis/moviedb/ui/fragment/theater/TheaterScreen.kt
+++ app/src/main/java/gr/pchasapis/moviedb/ui/fragment/theater/TheaterScreen.kt
@@ -1,29 +1,28 @@
 package gr.pchasapis.moviedb.ui.fragment.theater
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import gr.pchasapis.moviedb.R
 import gr.pchasapis.moviedb.model.data.MovieDataModel
@@ -39,7 +38,7 @@ fun TheatreScreen(
 
     Scaffold(
         topBar = {
-            ToolbarView(text = "Extra")
+            ToolbarView(text = "Movies in theatre")
         },
 
         modifier = modifier.fillMaxSize(),
@@ -76,34 +75,27 @@ private fun TheatreMainView(
 @Composable
 fun Content(list: List<MovieDataModel>) {
 
-    Column {
 
+    LazyVerticalGrid(
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        contentPadding = PaddingValues(horizontal = 10.dp, vertical = 10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        columns = GridCells.Adaptive(100.dp)
+    ) {
+        items(list) { item ->
 
-        Text(
-            "Movies in theatre",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(horizontal = 10.dp, vertical = 20.dp),
-        )
-
-        LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
-            contentPadding = PaddingValues(horizontal = 10.dp)
-        ) {
-            items(list) { item ->
-
-                AsyncImage(
-                    model = item.thumbnail,
-                    contentDescription = "",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .height(150.dp)
-                        .width(80.dp)
-                        .clip(RoundedCornerShape(10.dp)),
-                    placeholder = painterResource(id = R.mipmap.ic_launcher),
-                    error = painterResource(id = R.mipmap.ic_launcher)
-                )
-            }
+            AsyncImage(
+                model = item.thumbnail,
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(150.dp)
+                    .width(80.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color.White),
+                placeholder = painterResource(id = R.mipmap.ic_launcher),
+                error = painterResource(id = R.mipmap.ic_launcher)
+            )
         }
 
     }
