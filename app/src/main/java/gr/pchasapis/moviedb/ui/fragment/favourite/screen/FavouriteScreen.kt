e4348262be565d426149a6ee4648820358b97954
Pantelis Chasapis <panteleimon.chasapis1@vodafone.com>
2025-01-16 17:01:24 +0200

    wip filtering

--- app/src/main/java/gr/pchasapis/moviedb/ui/fragment/favourite/screen/FavouriteScreen.kt
+++ app/src/main/java/gr/pchasapis/moviedb/ui/fragment/favourite/screen/FavouriteScreen.kt
@@ -2,9 +2,15 @@
 
 package gr.pchasapis.moviedb.ui.fragment.favourite.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AssistChip
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import gr.pchasapis.moviedb.R
@@ -42,6 +49,7 @@ fun FavouriteRoute(
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun FavouriteScreen(
     state: FavouriteUiState,
@@ -52,11 +60,25 @@ fun FavouriteScreen(
 
 
     Scaffold(
+        containerColor = MaterialTheme.colorScheme.primary,
         topBar = {
-            ToolbarView(
-                scrollBehavior = scrollBehavior,
-                text = stringResource(id = R.string.favourite_screen)
-            )
+            Column {
+                ToolbarView(
+                    scrollBehavior = scrollBehavior,
+                    text = stringResource(id = R.string.favourite_screen)
+                )
+
+                FlowRow(
+                    modifier = Modifier
+                        .padding(horizontal = 14.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Chip("Date Added")
+                    Chip("Rate")
+                    Chip("Name")
+                }
+            }
         },
 
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -75,10 +97,29 @@ fun FavouriteScreen(
     )
 }
 
+@Composable
+fun Chip(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    AssistChip(
+        onClick = { },
+        modifier = modifier.padding(horizontal = 2.dp),
+        label = {
+            Text(
+                text,
+                color = Color.White
+            )
+        }
+    )
+}
+
 
 @Composable
-fun ToolbarView(scrollBehavior: TopAppBarScrollBehavior? = null,
-                text: String) {
+fun ToolbarView(
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    text: String
+) {
     CenterAlignedTopAppBar(
         title = { Text(text) },
         colors = TopAppBarDefaults.topAppBarColors(
@@ -87,7 +128,7 @@ fun ToolbarView(scrollBehavior: TopAppBarScrollBehavior? = null,
             titleContentColor = Color.White,
             scrolledContainerColor = MaterialTheme.colorScheme.primary
         ),
-        scrollBehavior = scrollBehavior
+        scrollBehavior = scrollBehavior,
     )
 }
 
